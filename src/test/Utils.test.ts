82887154ec0168388b4087a40a93516761212a0d
Anukul <anukul.p@growthsourceft.com>
2021-12-22 11:39:13 +0530

    project setup

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import {Utils} from "../app/Utils"
+
+describe('Utils test suit',()=>{
+    test('first test',()=>{
+        const abc = Utils.toUpperCase('abc');
+        console.log('tests work');
+    })
+})
