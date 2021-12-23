821bc18f2bc4f231180de2039fd45840608a9b6c
Anukul <anukul.p@growthsourceft.com>
2021-12-23 10:26:51 +0530

    added tests

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,5 +1,15 @@
-export class Utils{
-    public static toUpperCase(arg:string){
-        return '';
+import { UrlWithParsedQuery, parse } from 'url';
+
+export class Utils {
+  public static parseUrl(url: string): UrlWithParsedQuery {
+    if (!url) {
+      throw new Error('empty url');
     }
+    return parse(url, true);
+  }
+
+  /* istanbul ignore next */
+  public static toUpperCase(arg: string): string {
+    return arg.toUpperCase();
+  }
 }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,61 @@
-import {Utils} from "../app/Utils"
+import { Utils } from '../app/Utils';
 
-describe('Utils test suit',()=>{
-    test('first test',()=>{
-        const abc = Utils.toUpperCase('abc');
-        console.log('tests work');
-    })
-})
+describe('Utils test suit', () => {
+  beforeAll(() => {
+    console.log('before all');
+  });
+
+  beforeEach(() => {
+    console.log('before each');
+  });
+
+  test('first test', () => {
+    const result = Utils.toUpperCase('abc');
+    expect(result).toBe('ABC');
+    console.log('tests work');
+  });
+
+  test('parse simple url', () => {
+    const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+    expect(parsedUrl.href).toBe('http://localhost:8080/login');
+    expect(parsedUrl.port).toBe('8080');
+    expect(parsedUrl.protocol).toBe('http:');
+    expect(parsedUrl.query).toEqual({});
+  });
+
+  test('parse URL with query', () => {
+    const parsedUrl = Utils.parseUrl(
+      'http://localhost:8080/login?user=user&password=pass'
+    );
+    const expectedQuery = {
+      user: 'user',
+      password: 'pass',
+    };
+
+    expect(parsedUrl.query).toEqual(expectedQuery);
+    expect(expectedQuery).toBe(expectedQuery);
+  });
+
+  test.only('test invalid url', () => {
+    function expectError() {
+      Utils.parseUrl('');
+    }
+
+    expect(expectError).toThrowError('empty url');
+  });
+
+  test('test invalid url with arrow function', () => {
+    expect(() => {
+      Utils.parseUrl('');
+    }).toThrowError('empty url');
+  });
+
+  test.only('test invalid url with try catch', () => {
+    try {
+      Utils.parseUrl('');
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error).toHaveProperty('message', 'empty url');
+    }
+  });
+});
